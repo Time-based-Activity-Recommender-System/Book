56b8aee338807de6438860ed2ec0a4f01dad3141
Asmita Poddar <asmita_poddar@yahoo.co.in>
2017-04-06 07:20:46 +0530

    Update print_popular_books.py

--- print_popular_books.py
+++ print_popular_books.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 
 pop_cols = ['book_id', 'book_title']
-pop_movies = pd.read_csv('popular_books.data', sep=';', names=pop_cols, encoding='latin-1')
+pop_books = pd.read_csv('popular_books.data', sep=';', names=pop_cols, encoding='latin-1')
 l = len(pop_movies) 
 #print "Length = ", l
 ind1 = np.random.randint(0, l)
@@ -15,6 +15,6 @@ while(ind1 == ind2):
 while(ind1 == ind3 and ind2 == ind3):
 	ind3 = np.random.randint(0, l)
 
-print pop_movies['book_title'][ind1]
-print pop_movies['book_title'][ind2]
-print pop_movies['book_title'][ind3]
+print pop_books['book_title'][ind1]
+print pop_books['book_title'][ind2]
+print pop_books['book_title'][ind3]
