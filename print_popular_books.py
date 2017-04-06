a595fc8bc7450f2f1c38d104e5102a951f1b3fdd
Asmita Poddar <asmita_poddar@yahoo.co.in>
2017-04-06 07:10:29 +0530

    Add files via upload

---
+++ print_popular_books.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+
+pop_cols = ['book_id', 'book_title']
+pop_movies = pd.read_csv('popular_books.data', sep=';', names=pop_cols, encoding='latin-1')
+l = len(pop_movies) 
+#print "Length = ", l
+ind1 = np.random.randint(0, l)
+ind2 = np.random.randint(0, l)
+ind3 = np.random.randint(0, l)
+
+
+while(ind1 == ind2):
+	ind2 = np.random.randint(0, l)
+while(ind1 == ind3 and ind2 == ind3):
+	ind3 = np.random.randint(0, l)
+
+print pop_movies['book_title'][ind1]
+print pop_movies['book_title'][ind2]
+print pop_movies['book_title'][ind3]
